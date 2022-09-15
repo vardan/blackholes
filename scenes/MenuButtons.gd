a25a7d1024fc33a2eb6b7af63c60aa7f7e076866
Vardan Saakian <saakyanv@gmail.com>
2022-09-15 20:49:13 +0200

    Fix assets

--- scenes/MenuButtons.gd
+++ scenes/MenuButtons.gd
@@ -9,7 +9,7 @@ func _on_Quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_Start_pressed() -> void:
-	add_child(game.instance())
+	get_tree().change_scene("res://scenes/Game.tscn")
 
 func _on_Settings_pressed() -> void:
 	get_node("MenuNode").move(Vector2(-576, 0))
